48f97bc58dc3062dc0fb646d37e386bd3169b0a4
bibekdhkl <bibekdh2016@gmail.com>
2025-01-22 15:26:22 +0545

    :white_check_mark:Added UseState Changes

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,13 +3,24 @@ import styles from "./ContactForm.module.css";
 import { MdMessage } from "react-icons/md";
 import { IoCallSharp } from "react-icons/io5";
 import { IoIosMail } from "react-icons/io";
+import { useState } from "react";
 
 const ContactForm = () => {
+      const [name,setName] = useState("Bibek");
+      const [email,setEmail] = useState("bibekdh2016@gmail.com");
+      const [text,setText] = useState("Hello Everyone");
 
- const onSubmitButton = (event) => {
-      event.preventDefault();
-      console.log(event)
- }
+  const onSubmitButton = (event) => {
+    event.preventDefault();
+    setName(event.target[0].value);
+    setEmail(event.target[1].value);
+    setText(event.target[2].value);
+//     email = event.target[1].value;
+//     text = event.target[2].value;
+
+    console.log(name,email,text)
+  };
+
   return (
     <section className={styles.container}>
       <div className={styles.contact_form}>
@@ -46,10 +57,13 @@ const ContactForm = () => {
           >
             <Button text="SUBMIT" />
           </div>
+          <div>
+            {name + " " + email + " " + text}
+          </div>
         </form>
       </div>
       <div className={styles.contact_image}>
-            <img src="./images/Contact-Us-Vector-Illustration-Part-02-1.jpg"></img>
+        <img src="./images/Contact-Us-Vector-Illustration-Part-02-1.jpg"></img>
       </div>
     </section>
   );
